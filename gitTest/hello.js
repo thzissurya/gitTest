54c0f776b0086d5027bbc382dadb8652cb83e670
thzissurya <suryaprakash4898@gmail.com>
2025-05-31 12:25:16 +0530

    Java Script file updated

--- gitTest/hello.js
+++ gitTest/hello.js
@@ -1,3 +1,23 @@
-for(let i=0; i<=10; i++){
-
+function multiplier(factor){
+    return function(number){
+    return number*factor
+};
 }
+
+function mapArray(array, transform){
+    const num= [];
+    for(let i=0; i< array.length; i++){
+        num.push(transform(array[i]));
+    }
+    return num;
+}
+
+const doublt= multiplier(2);
+const triplr = multiplier(3);
+
+const number= [1, 2, 3, 4, 5 ];
+const doublenumber= mapArray(number, doublt);
+const tripledNumber= mapArray(number, triplr);
+
+console.log(doublenumber);
+console.log(tripledNumber);
